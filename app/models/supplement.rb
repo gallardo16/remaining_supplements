eca520276528e85aba039ff79cfe5cf7706b6660
gallardo <aiki.hosogaya@gmail.com>
2021-05-19 17:59:36 +0900

    残量となくなる日を正しく表示させた

--- app/models/supplement.rb
+++ app/models/supplement.rb
@@ -1,2 +1,17 @@
 class Supplement < ApplicationRecord
+  def remaining_quantity
+    content_size - total_intake.to_i
+  end
+
+  def total_intake
+    day_of_keep_taking_supplement * daily_intake
+  end
+
+  def day_of_keep_taking_supplement
+    Date.today.strftime('%F').to_i - updated_at.strftime('%F').to_i
+  end
+
+  def empty_date
+    updated_at.next_day(content_size / daily_intake).strftime('%F')
+  end
 end
