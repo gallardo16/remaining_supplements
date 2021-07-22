143efb1be472fe19e2c0b67867bf7eab4a019717
gallardo <aiki.hosogaya@gmail.com>
2021-07-22 17:49:11 +0900

    <WIP>supplement管理テスト一部を除いて追加

---
+++ spec/factories/supplement.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :supplement do
+    name { 'foo' }
+    supplement_type { 'tablet' }
+    content_size { 100 }
+    daily_intake { 1 }
+    remind { 2 }
+    user_id { 1 }
+  end
+end
