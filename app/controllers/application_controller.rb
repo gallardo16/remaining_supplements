e13c23cd9b97cc5065315b8344fef579276f6665
gallardo <aiki.hosogaya@gmail.com>
2021-07-28 15:24:44 +0900

    topページが正しく表示されるように修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
-
   def after_sign_up_path_for(resource)
     supplements_path
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   devise_for :users
-  root 'home#index'
+  root 'supplements#index'
 
   get 'home', to: 'home#index'
   resources :supplements
