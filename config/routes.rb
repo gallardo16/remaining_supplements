b542ef6e3b98a39bebb2ab455d3131f4a5820a03
gallardo <aiki.hosogaya@gmail.com>
2021-07-31 21:32:46 +0900

    ルーティングを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
-  root "supplements#index"
-
   get "home", to: "home#index"
-  resources :supplements
-
   get "tos", to: "tos#index"
   get "policy", to: "policy#index"
+
+  authenticated :user do
+    root "supplements#index", :as => "user_authenticated_root"
+  end
+  root "home#index"
+
+  devise_for :users
+  resources :supplements
+
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
