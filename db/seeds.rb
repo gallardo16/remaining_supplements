4cf9f3c57ae9140c457ff403adf0e1003ca4469e
gallardo <aiki.hosogaya@gmail.com>
2021-07-31 17:02:13 +0900

    seed値を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,36 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user = User.create!(
+  email: "test@example.com",
+  password: "testtest"
+)
+
+user.supplements.create!(
+  name: "tablet1",
+  supplement_type: "tablet",
+  content_size: 100,
+  daily_intake: 1,
+  remind: "1日前"
+)
+
+user.supplements.create!(
+  name: "tablet2",
+  supplement_type: "tablet",
+  content_size: 200,
+  daily_intake: 20,
+  remind: "2日前"
+)
+
+user.supplements.create!(
+  name: "powder1",
+  supplement_type: "powder",
+  content_size: 1000,
+  daily_intake: 10,
+  remind: "1週間前"
+)
+user.supplements.create!(
+  name: "powder2",
+  supplement_type: "powder",
+  content_size: 5000,
+  daily_intake: 100,
+  remind: "2週間前"
+)
