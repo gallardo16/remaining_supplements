5b30d0bad4385c5646945bd00b8e1a1668a1b380
gallardo <aiki.hosogaya@gmail.com>
2021-09-18 17:09:30 +0900

    不要なコードを削除

--- app/controllers/api/supplements_controller.rb
+++ app/controllers/api/supplements_controller.rb
@@ -1,10 +1,6 @@
 class Api::SupplementsController < ApiController
   before_action :authenticate_user!
 
-  rescue_from ActiveRecord::RecordNotFound do |exception|
-      render json: { error: '404 not found' }, status: 404
-  end
-
   def index
     @supplements = current_user.supplements
   end
